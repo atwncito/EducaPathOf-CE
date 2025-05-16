46b515db9b01b005a407cf3910cd9708439ac4c3
Rodrigo Antùnez <rodrigo.antunez@estudiantes.utec.edu.uy>
2025-05-16 13:23:54 -0300

    Se definio la clase Usuario con sus atributos, constructor y metodos. Se definieron las subclases Estudiante, Docente, Psicopedagogo, Administrador y Auditor.

---
+++ src/CasoEstudioUTEC/Auditor.java
@@ -0,0 +1,28 @@
+package CasoEstudioUTEC;
+
+public class Auditor extends Usuario {
+    private String areaAsignada;
+
+    // Constructor principal de la clase.
+    public Auditor(String nombre, String email, String contrasenia, String areaAsignada) {
+        super(nombre, email, contrasenia);
+        this.areaAsignada = areaAsignada;
+    }
+
+    // Metodo Get y Set.
+    public String getAreaAsignada() {
+        return areaAsignada;
+    }
+    public void setAreaAsignada(String areaAsignada) {
+        this.areaAsignada = areaAsignada;
+    }
+
+    @Override
+    public String toString() {
+        return "Auditor{" +
+                "nombre='" + getNombre() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", areaAsignada='" + areaAsignada + '\'' +
+                '}';
+    }
+}
